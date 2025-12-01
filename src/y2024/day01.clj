ac1ef306337286a5527ca2e8144b0c3fe9dc5914
ktunprasert <ktunprasert@outlook.com>
2025-12-01 22:17:48 +0000

    2024.1: washed harder

--- src/y2024/day01.clj
+++ src/y2024/day01.clj
@@ -3,25 +3,33 @@
             [clojure.java.io :as io]
             [clojure.pprint :refer [pprint]]))
 
+; (defn parse [fname]
+;   (->>  (with-open [r (io/reader fname)] (doall (line-seq r)))
+;         (map #(->> (str/split % #"\s{3}")
+;                    (map Integer/parseInt)))))
+
 (defn parse [fname]
-  (->>  (with-open [r (io/reader fname)] (doall (line-seq r)))
-        (map #(->> (str/split % #"\s{3}")
-                   (map Integer/parseInt)))))
+  (-> (io/file fname)
+      slurp
+      str/split-lines
+      (->> (map (fn [line] (->> (str/split line #"\s{3}") (mapv parse-long))))
+           vec)))
 
 (defn solve [vec]
-  (let [unzipped (apply map vector vec)
-        sorted (map sort unzipped)
-        zipped (apply map vector sorted)
-        v (first unzipped)
-        freq (frequencies (second unzipped))]
-    (-> [(->> (map (fn [[a b]] (abs (- a b))) zipped) (apply +))
-         (->> (map (fn [n] (* n (get freq n 0))) v) (apply +))]
-        pprint)))
+  (let [[v1 v2] (apply map vector vec)
+        sorted-pairs (map sort [v1 v2])
+        zipped (apply map vector sorted-pairs)
+        freq (frequencies v2)
+
+        sum-abs-dist (reduce + (map (fn [[a b]] (abs (- a b))) zipped))
+        sum-freq-prod (reduce + (map (fn [n] (* n (get freq n 0))) v1))]
+    [sum-abs-dist sum-freq-prod]))
 
 (defn -main [& args]
   (->> args
        first
        parse
-       solve))
+       solve
+       pprint))
 
 (apply -main *command-line-args*)
